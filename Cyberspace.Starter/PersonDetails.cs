c9d9353fa63fd5153fdd234fb13d219e1bb5f31d
olabisi09 <olabisiolaoye09@gmail.com>
2022-04-06 09:48:24 +0100

    Demonstrating concepts of inheritance

---
+++ Cyberspace.Starter/PersonDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyberspace.Starter
+{
+    public class PersonDetails
+    {
+        public string name = "Olabisi";
+        public int age = 20;
+        public bool hasKids = false;
+    }
+}
