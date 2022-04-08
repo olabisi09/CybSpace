6c765f06c6016e6edb782e7c88e8abe8b8963a80
olabisi09 <olabisiolaoye09@gmail.com>
2022-04-08 10:20:13 +0100

    Changed some stuff

--- Cyberspace.Starter/Program.cs
+++ Cyberspace.Starter/Program.cs
@@ -3,16 +3,50 @@ using Cyberspace.Library;
 
 namespace Cyberspace.Starter
 {
+    public class Book
+    {
+        public string title;
+        public string author;
+        public string ISBN;
+
+        public void ReadBook()
+        {
+            Console.WriteLine("This book can been read.");
+        }
+
+        public void ShelveBook()
+        {
+            Console.WriteLine("This book has been shelved.");
+        }
+    }
+
     public class Program
     {
         public static void Main()
         {
-            Circle.Area(5.6);
-            Employee.SignIn();
+            Car car = new Car(); //Create an instance of the class
+            car.DisplayVehicleType("Corolla"); //Inherits the DisplayVehicleType method from Vehicle
+            car.GetBrandName();
 
-            Circle x = new Circle();
-            Circle y = new();
-            var z = new Circle();
+            Truck truck = new Truck(); //Create an instance of the class
+            truck.DisplayVehicleType("Volkswagen"); //Inherits the DisplayVehicleType method from Vehicle
+            truck.GetBrandName();
+
+            Motorcycle motorcycle = new Motorcycle(); //Create an instance of the class
+            motorcycle.DisplayVehicleType("Vespa"); //Inherits the DisplayVehicleType method from Vehicle
+            motorcycle.GetBrandName();
+
+
+            //Book someBook = new Book();
+
+            //someBook.title = "The Hobbit";
+            //someBook.author = "J.R.R Tolkien";
+            //someBook.ISBN = "FR-543";
+
+            //someBook.ReadBook();
+            //someBook.ShelveBook();
+            //Console.WriteLine("Title: {0}\nAuthor: {1}\nISBN: {2}", someBook.title, someBook.author, someBook.ISBN);
         }
+        
     }
 }
