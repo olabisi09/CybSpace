08feb33053d64c520c49c19f14ca64020ac3258f
olabisi09 <olabisiolaoye09@gmail.com>
2022-04-08 13:54:21 +0100

    Structures, Interfaces, Properties and stuff

--- Cyberspace.Starter/Program.cs
+++ Cyberspace.Starter/Program.cs
@@ -3,50 +3,53 @@
 
 namespace Cyberspace.Starter
 {
-    public class Book
-    {
-        public string title;
-        public string author;
-        public string ISBN;
-
-        public void ReadBook()
-        {
-            Console.WriteLine("This book can been read.");
-        }
-
-        public void ShelveBook()
-        {
-            Console.WriteLine("This book has been shelved.");
-        }
-    }
-
     public class Program
     {
         public static void Main()
         {
-            Car car = new Car(); //Create an instance of the class
-            car.DisplayVehicleType("Corolla"); //Inherits the DisplayVehicleType method from Vehicle
-            car.GetBrandName();
-
-            Truck truck = new Truck(); //Create an instance of the class
-            truck.DisplayVehicleType("Volkswagen"); //Inherits the DisplayVehicleType method from Vehicle
-            truck.GetBrandName();
-
-            Motorcycle motorcycle = new Motorcycle(); //Create an instance of the class
-            motorcycle.DisplayVehicleType("Vespa"); //Inherits the DisplayVehicleType method from Vehicle
-            motorcycle.GetBrandName();
-
-
-            //Book someBook = new Book();
-
-            //someBook.title = "The Hobbit";
-            //someBook.author = "J.R.R Tolkien";
-            //someBook.ISBN = "FR-543";
-
-            //someBook.ReadBook();
-            //someBook.ShelveBook();
-            //Console.WriteLine("Title: {0}\nAuthor: {1}\nISBN: {2}", someBook.title, someBook.author, someBook.ISBN);
+            var p = new Point[9];
+            var line = new Line(p);
+            
         }
         
     }
+
+    public interface ICompress
+    {
+        int Read();
+    }
+    public interface IDrive
+    {
+        int Write();
+    }
+
+    public struct Line
+    {
+        public Line(Point[] points)
+        {
+            Points = points;
+        }
+        public Point[] Points { get; set; }
+    }
+
+    public enum Gender
+    {
+        male, female
+    }
+    public enum UnitTypes
+    {
+        Millimeters = 12, Centimeters = 25
+    }
+    public class Point
+    {
+        public UnitTypes unit { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public override string ToString()
+        {
+            return ($"X = {X} {unit}, Y = {Y} {unit}");
+        }
+    }
+
 }
