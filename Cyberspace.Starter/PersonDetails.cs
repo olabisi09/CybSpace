152401ad38e1755b88f803e93b58ec644138556a
olabisi09 <olabisiolaoye09@gmail.com>
2022-04-11 14:31:52 +0100

    Generics classwork

--- Cyberspace.Starter/PersonDetails.cs
+++ Cyberspace.Starter/PersonDetails.cs
@@ -12,4 +12,73 @@ namespace Cyberspace.Starter
         public int age = 20;
         public bool hasKids = false;
     }
+    
+    public class Addition<E, F>
+    {
+        public void Add(E par1, F par2, bool x)
+        {
+
+        }
+    }
+
+    //public class Shape<T>
+    //{
+    //    public T Par1
+    //    {
+    //        get;
+    //    }
+    //}
+
+    //public class Circle<T> : Shape<T>
+    //{
+    //    public Circle(T num)
+    //    {
+    //    }
+    //}
+
+    //public interface IData
+    //{
+    //    void Add<T>(T par1);
+    //}
+
+    //public class Data<T> : IData
+    //{
+    //    private T[] _data;
+    //    private int _index;
+
+    //    public Data()
+    //    {
+    //        _data = new T[10];
+    //    }
+
+    //    public void Add<D, E>(D par1)
+    //    {
+    //        _data[_index++] = par1;
+    //    }
+    //}
 }
+//Question 131: An interface defines or declares the methods, fields and properties that the
+//class will then implement.
+
+//Question 132: An abstract base class can only be inherited by an abstract class,
+//and all of them must be inherited.
+
+//Question 133: class NameOfClass : MyBase, ISuppose, IDo{}
+
+//Question 134: is and as operators
+
+//Question 135: the 'is' operator compares two types to determine if they are compatible.
+//If they are not, false is returned. The 'as' operator is casting from one type to another.
+//If the types are not compatible, null is returned, otherwise the resultant object type is returned.
+
+//Question 136: An interface extends another interface by implementing its properties and
+//methods, and can also add its own.
+
+//Question 137: interface MyInterface : AnotherInterface {}
+
+//Question 138: When an interface extends another and implements a method in the parent
+//
+//it, then they can be overridden in derived classes.
+
+//Question 139: This is implementing an interface by writing its name along with its methods.
+//This is to avoid confusion with methods in different interfaces with the same signatures.
--- Cyberspace.Starter/Program.cs
+++ Cyberspace.Starter/Program.cs
@@ -3,123 +3,46 @@
 
 namespace Cyberspace.Starter
 {
-    public interface IConvertible
+   public class ShoppingBag<T> where T : Bag
     {
-        public string ConvertToCSharp(string val);
-        public string ConvertToVB2005(string val);
-    }
-
-    public class ProgramHelper : ProgramConverter, IOtherInterface
-    {
-        public bool CodeCheckSyntax(string stringToCheck, string langToUse)
+        public T[] items = new T[5];
+        int _index = 0;
+        public void AddItem(T bags)
         {
-            if (langToUse == "CSharp")
-            {
-                Console.WriteLine($"Check if the string '{stringToCheck}' is in C#.");
-                return true;
-            }
-            else if (langToUse == "VB2005")
-            {
-                Console.WriteLine($"Check if the string '{stringToCheck}' is in VB2005.");
-                return true;
-            }
+            items[_index++] = bags;
+        }
 
-            return false;
+        public T GetItem(int index)
+        {
+            return items[index];
         }
     }
 
-
-    //public interface ICompress
-    //{
-    //    void Write();
-    //}
-    //public interface IStore:ICompress
-    //{
-    //    void Write();
-    //}
-
-    //public class StoreCompress : IStore 
-    //{
-    //    public void Write()
-    //    {
-
-    //    }
-
-    //    void ICompress.Write()
-    //    {
-
-    //    }
-    //}
-
-    ////public class ProgramA { }
-
-    ////public abstract class ProgramB : ProgramA { }
-
-    public interface IOtherInterface : IConvertible
+    public class Bag
     {
-        public bool CodeCheckSyntax(string stringToCheck, string langToUse);
-    }
+        public string Name { get; set; }  
+        public int Count { get; set; }
 
-    public class ProgramConverter : IConvertible
-    {
-        public string ConvertToCSharp(string val)
+        public override string ToString()
         {
-            return $"{val} converted to C#";
-        }
-
-        public string ConvertToVB2005(string val)
-        {
-            return $"{val} converted to VB 2005";
+            return $"Name: {Name}\nCount: {Count}";
         }
     }
-
-    
 
     public class Program
     {
-        public static void Main()
+        static void Main()
         {
-            ProgramHelper programHelper = new ProgramHelper();
-            string cSharpString = programHelper.ConvertToCSharp("Some string");
-            string vbString = programHelper.ConvertToVB2005("Other string");
-
-            //Implementing Exercise 13-2 and 13-3
-            Console.WriteLine(cSharpString);
-            Console.WriteLine(vbString);
-
-            programHelper.CodeCheckSyntax(cSharpString, "CSharp");
-            programHelper.CodeCheckSyntax(vbString, "VB2005");
-
-            //Testing the is and as keywords
-            ProgramConverter[] programConverters = new ProgramConverter[2];
-            programConverters[0] = new ProgramConverter();
-            programConverters[1] = new ProgramHelper();
-
-            //Demonstrating the 'is' keyword
-            for (int i = 0; i < programConverters.Length; i++)
+            var shoppingBag = new ShoppingBag<Bag>();
+            shoppingBag.AddItem(new Bag() { Name = "Spaghetti", Count = 20});
+            shoppingBag.AddItem(new Bag() { Name = "Rice", Count = 34 });
+            var counter = 0;
+            while (counter < 5)
             {
-                Console.WriteLine(programConverters[i] is ProgramHelper);
+                Console.WriteLine(shoppingBag.GetItem(counter));
+                counter++;
             }
-
-            //Demonstrating the 'as' keyword
-            for (int i = 0; i < programConverters.Length; i++)
-            {
-                ProgramHelper ph = programConverters[i] as ProgramHelper;
-                if (ph != null)
-                {
-                    ph.CodeCheckSyntax(vbString, "VB2005");
-                }
-                else Console.WriteLine("Not VB syntax");
-
-                if(ph != null)
-                {
-                    ph.CodeCheckSyntax(cSharpString, "CSharp");
-                }
-                else Console.WriteLine("Not C# syntax");
-
-                
-            } 
-
+            shoppingBag.GetItem(3);
         }
         
     }
@@ -127,28 +50,4 @@ namespace Cyberspace.Starter
 }
 
 
-//Question 131: An interface defines or declares the methods, fields and properties that the
-//class will then implement.
 
-//Question 132: An abstract base class can only be inherited by an abstract class,
-//and all of them must be inherited.
-
-//Question 133: class NameOfClass : MyBase, ISuppose, IDo{}
-
-//Question 134: is and as operators
-
-//Question 135: the 'is' operator compares two types to determine if they are compatible.
-//If they are not, false is returned. The 'as' operator is casting from one type to another.
-//If the types are not compatible, null is returned, otherwise the resultant object type is returned.
-
-//Question 136: An interface extends another interface by implementing its properties and
-//methods, and can also add its own.
-
-//Question 137: interface MyInterface : AnotherInterface {}
-
-//Question 138: When an interface extends another and implements a method in the parent
-//
-//it, then they can be overridden in derived classes.
-
-//Question 139: This is implementing an interface by writing its name along with its methods.
-//This is to avoid confusion with methods in different interfaces with the same signatures.
