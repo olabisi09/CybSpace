36595e3463c0a4972a6e7eddcf8132750a512022
olabisi09 <olabisiolaoye09@gmail.com>
2022-04-13 14:11:51 +0100

    My experiment on events

--- Cyberspace.Starter/Program.cs
+++ Cyberspace.Starter/Program.cs
@@ -3,73 +3,39 @@
 
 namespace Cyberspace.Starter
 {
-    public delegate int MyDelegate(int x, int y);
-    public delegate int Arithmetic<Z>(Z x, Z y);
+    public delegate void Notify();
 
-    //Classwork
-    public delegate int ArithmeticOperationDelegate(int a, int b);
-
-    public class ArithmeticOperation
+    public class Container
     {
-        public static int Add(int i, int j)
+        public event Notify Notify;
+
+        public void NotifyStart()
         {
-            return i + j;
+            Console.WriteLine("Some special event happened.");
+            OnNotify();
         }
-        public static int Minus(int i, int j)
+
+        protected virtual void OnNotify()
         {
-            return i - j;
-        }
-        public static int Multiply(int i, int j)
-        {
-            return i * j;
+            Notify.Invoke();
         }
     }
+
     public class Program
     {
         
         static void Main()
         {
-            var myArray = new ArithmeticOperationDelegate[3];
-            int i = 0;
+            Container container = new Container();
+            container.Notify += NotifyFinish;
+            container.NotifyStart();
 
-            //Using a for loop
-
-            //for (int i = 0; i < myArray.Length; i++)
-            //{
-            //    if (i == 0)
-            //    {
-            //        myArray[i] = ArithmeticOperation.Add;
-            //    }
-            //    else if (i == 1)
-            //    {
-            //        myArray[i] = ArithmeticOperation.Minus;
-            //    }
-            //    else if (i == 2)
-            //    {
-            //        myArray[i] = ArithmeticOperation.Multiply;
-            //    }
-
-            //    var result = myArray[i];
-
-            //    Console.WriteLine("Method: " + result(50, 40));
-            //}
-
-            //Using a switch statement
-            switch (i)
+            static void NotifyFinish() 
             {
-                case 0:
-                    myArray[i] = ArithmeticOperation.Add;
-                    Console.WriteLine("Method: " + myArray[i](50, 40));
-                    goto case 1;
-                case 1:
-                    myArray[i] = ArithmeticOperation.Minus;
-                    Console.WriteLine("Method: " + myArray[i](50, 40));
-                    goto default;
-                default:
-                    myArray[i] = ArithmeticOperation.Multiply;
-                    Console.WriteLine("Method: " + myArray[i](50, 40));
-                    break;
+                Console.WriteLine("All done with the event.");
             }
+
+            
             
         }
     }
