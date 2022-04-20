c8bc4d5d941cf075095d95bea7cd89384ece24a0
olabisi09 <olabisiolaoye09@gmail.com>
2022-04-20 12:36:03 +0100

    stuff

--- Cyberspace.Starter/Program.cs
+++ Cyberspace.Starter/Program.cs
@@ -2,95 +2,33 @@
 
 namespace Cyberspace.Starter
 {
-    public delegate int Arithmetic<Z>(Z x, Z y);
-
-    //Classwork
-    public delegate int ArithmeticOperationDelegate(int a, int b);
-
-    public class ArithmeticOperation
+    public static class Extension
     {
-        public static int Add(int i, int j)
+        public static bool isValidTelNo(this string x)
         {
-            return i + j;
-        }
-        public static int Minus(int i, int j)
-        {
-            return i - j;
-        }
-        public static int Multiply(int i, int j)
-        {
-            return i * j;
+            return x.Length == 11 ? true : false;
         }
 
-        public static void Method()
+        public static int SearchByKeyword(this string input, string key)
         {
-
-        }
-        public static int Method2(int a)
-        {
-            return 0;
+            return input.IndexOf(key, StringComparison.InvariantCultureIgnoreCase);
         }
     }
     public class Program
     {
         static void Main()
         {
-            
-
-            Action action = ArithmeticOperation.Method;
-            Func<int, int> func = ArithmeticOperation.Method2;
-
-            Func<int, bool, string> func1 = (a, b) =>
-            {
-                return "something";
-            };
-
-            var myArray = new ArithmeticOperationDelegate[3];
-            //or var myArray = new Func<int, int, int>[3];
-
-
-            //Using a for loop
-
-            //for (int i = 0; i < myArray.Length; i++)
+            //Console.Write("Enter something: ");
+            //string s = Console.ReadLine();
+            //if (s.isValidTelNo())
             //{
-            //    if (i == 0)
-            //    {
-            //        myArray[i] = ArithmeticOperation.Add;
-            //    }
-            //    else if (i == 1)
-            //    {
-            //        myArray[i] = ArithmeticOperation.Minus;
-            //    }
-            //    else if (i == 2)
-            //    {
-            //        myArray[i] = ArithmeticOperation.Multiply;
-            //    }
-
-            //    var result = myArray[i];
-
-            //    Console.WriteLine("Method: " + result(50, 40));
+            //    Console.WriteLine(s);
             //}
 
-            //Using a switch statement
-
-            int i = 0;
-            switch (i)
-            {
-                case 0:
-                    myArray[i] = ArithmeticOperation.Add;
-                    Console.WriteLine("Method: " + myArray[i](50, 40));
-                    goto case 1;
-                case 1:
-                    myArray[i] = ArithmeticOperation.Minus;
-                    Console.WriteLine("Method: " + myArray[i](50, 40));
-                    goto default;
-                default:
-                    myArray[i] = ArithmeticOperation.Multiply;
-                    Console.WriteLine("Method: " + myArray[i](50, 40));
-                    break;
-            }
-
-
+            string sentence = "This is a boy";
+            string searchKeyword = Console.ReadLine();
+            int index = sentence.SearchByKeyword(searchKeyword);
+            Console.WriteLine(index);
         }
     }
 
