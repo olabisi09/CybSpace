997c3d51d92d2a46009e63b766634a5b7e66587f
olabisi09 <olabisiolaoye09@gmail.com>
2022-04-22 22:07:04 +0100

    IComparable assignment

---
+++ Cyberspace.Starter/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyberspace.Starter
+{
+    public class Product : IComparable<Product>
+    {
+        public string Name { get; set; }   
+        public int Quantity { get; set; }
+
+        public int CompareTo(Product other)
+        {
+            if (other == null) return 1;
+            return Quantity.CompareTo(other.Quantity);
+        }
+
+        //public int CompareTo(object obj)
+        //{
+        //    if (obj == null) return 1;
+        //    Product product = obj as Product;
+        //    if (product == null) return -1;
+        //    else return 0;
+        //}
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Quantity: {Quantity}";
+        }
+    }
+}
