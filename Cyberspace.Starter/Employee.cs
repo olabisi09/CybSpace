16a3189a0a2cfdebc8c2bcd76e8c5baf5dbdc4b6
olabisi09 <olabisiolaoye09@gmail.com>
2022-04-28 09:28:34 +0100

    LINQ Join

--- Cyberspace.Starter/Employee.cs
+++ Cyberspace.Starter/Employee.cs
@@ -8,10 +8,51 @@ namespace Cyberspace.Starter
 {
     public class Employee
     {
-        public int Id { get; set; }
+        public int RollNumber { get; set; }
         public string Name { get; set; }    
         public int Age { get; set; }
-        public string Gender { get; set; }
-        public double Salary { get; set; }
+        public Gender Gender { get; set; }
+        public int Designation { get; set; }
+
+        public static List<Employee> GetEmployees()
+        {
+            return new List<Employee>
+            {
+                new Employee() { RollNumber = 101, Name = "Monica Geller", Age = 23, Gender = Gender.female, Designation = 3},
+                new Employee() { RollNumber = 102, Name = "Ross Geller", Age = 28, Gender = Gender.male, Designation = 3},
+                new Employee() { RollNumber = 103, Name = "Phoebe Buffay", Age = 30, Gender = Gender.female, Designation = 1},
+                new Employee() { RollNumber = 104, Name = "Chandler Bing", Age = 29, Gender = Gender.male, Designation = 2},
+                new Employee() { RollNumber = 105, Name = "Joey Tribbiani", Age = 35, Gender = Gender.male, Designation = 4},
+                new Employee() { RollNumber = 106, Name = "Eddie Moskowitz", Age = 32, Gender = Gender.male, Designation = 1},
+                new Employee() { RollNumber = 107, Name = "Joshua Burgen", Age = 40, Gender = Gender.male, Designation = 4},
+                new Employee() { RollNumber = 108, Name = "Janice Hosenstein", Age = 34, Gender = Gender.female, Designation = 2},
+                new Employee() { RollNumber = 109, Name = "Will Culvert", Age = 26, Gender = Gender.male, Designation = 2},
+                new Employee() { RollNumber = 110, Name = "Rachel Green", Age = 30, Gender = Gender.female, Designation = 3}
+            };
+        }
+    }
+
+    public enum Gender
+    {
+        male,
+        female
+    }
+
+    public class Salary
+    {
+        public int Id { get; set; }
+        public string LineManager { get; set; }
+        public double SalaryAmount { get; set; }
+
+        public static List<Salary> GetSalaries()
+        {
+            return new List<Salary> 
+            {
+                new Salary{Id = 1, LineManager = "Joanna Bush", SalaryAmount = 3500},
+                new Salary{Id = 2, LineManager = "John Doe", SalaryAmount = 5000},
+                new Salary{Id = 3, LineManager = "Kyle Libbett", SalaryAmount = 7000},
+                new Salary{Id = 4, LineManager = "Lonnie Litt", SalaryAmount = 4760},
+            };
+        }
     }
 }
--- Cyberspace.Starter/Program.cs
+++ Cyberspace.Starter/Program.cs
@@ -6,85 +6,99 @@ namespace Cyberspace.Starter
 {
     public class Program
     {
-        static string[] words = { "uno", "dos", "tres", "cuarto", "cinco" };
         static void Main()
         {
-            //var z = new List<Items>()
-            //{
-            //    new Items() {Id = 1, Name = "Book", Price = 1200, Quantity = 9},
-            //    new Items() {Id = 2, Name = "Pen", Price = 350.50, Quantity = 20},
-            //    new Items() {Id = 3, Name = "Glue", Price = 200, Quantity = 15},
-            //};
+            var employees = Employee.GetEmployees();
+            var lineManager = Salary.GetSalaries();
 
-            ////var res = z.Where(x => x.Quantity < 50)
-            ////    .OrderByDescending(x => x.Price)
-            ////    .ThenBy(x => x.Name)
-            ////    .Select(x => (name: x.Name, quantity: x.Quantity));
+            mylabel1:
+            //Left outer join: query syntax
+            var leftOuterJoinQuery = from x in lineManager
+                                 join y in employees
+                                 on x.Id equals y.Designation into groupedEmployees
+                                 from g in groupedEmployees.DefaultIfEmpty()
+                                 select new
+                                 {
+                                     x.Id,
+                                     x.LineManager,
+                                     groupedEmployees = g == null ? "No employees in this category." : g.Name,
+                                     groupedNumber = g == null ? 0 : g.RollNumber
+                                 };
+            goto mylabel1;
 
-            //var dict = z.GroupBy(x => x.Name).Select(x => (x.Key, price: x.Select(x => x.Price)));
-            //foreach (var item in dict)
-            //{
-            //    Console.WriteLine($"{item.Key}");
-            //    foreach (var item2 in item.price)
-            //    {
-            //        Console.WriteLine($"\tName: {item2}");
-            //    }
-            //}
+            //Group join: query syntax
+            var groupJoinQuery = from x in lineManager
+                              join y in employees
+                              on x.Id equals y.Designation
+                              into groupedEmployees
+                              select new
+                              {
+                                  x.Id,
+                                  x.LineManager,
+                                  groupedEmployees
+                              };
 
-            var employees = new List<Employee>(){
-                new Employee() { Id = 1, Name = "Monica Geller", Age = 23, Gender = "female", Salary = 3500},
-                new Employee() { Id = 2, Name = "Ross Geller", Age = 28, Gender = "male", Salary = 3000},
-                new Employee() { Id = 3, Name = "Phoebe Buffay", Age = 30, Gender = "female", Salary = 4500},
-                new Employee() { Id = 4, Name = "Chandler Bing", Age = 29, Gender = "male", Salary = 5500},
-                new Employee() { Id = 5, Name = "Joey Tribbiani", Age = 35, Gender = "male", Salary = 6370},
-                new Employee() { Id = 6, Name = "Eddie Moskowitz", Age = 32, Gender = "male", Salary = 2400},
-                new Employee() { Id = 7, Name = "Joshua Burgen", Age = 40, Gender = "male", Salary = 6400},
-                new Employee() { Id = 8, Name = "Janice Hosenstein", Age = 34, Gender = "female", Salary = 4000},
-                new Employee() { Id=9, Name = "Will Culvert", Age = 26, Gender = "male", Salary = 6250},
-                new Employee() { Id = 10, Name = "Rachel Green", Age = 30, Gender = "female", Salary = 7000}
-            };
-
-            var olderThan25 = employees.Where(employee => employee.Age > 25).OrderByDescending(employee => employee.Name).Select(employee => employee);
-            Console.WriteLine("Employees older than 25:");
-            foreach(var result2 in olderThan25)
+            foreach (var item in leftOuterJoinQuery)
             {
-                Console.WriteLine($"Name: {result2.Name}, Age: {result2.Age}");
-            }
-            Console.WriteLine();
-
-            var femaleEmployees = employees.Where(employee => employee.Gender == "female").Select(employee => employee);
-            Console.WriteLine("Female employees:");
-            foreach (var result3 in femaleEmployees)
-            {
-                Console.WriteLine($"Name: {result3.Name}");
-            }
-            Console.WriteLine();
-
-            var notTheseAges = employees.Where(employee => employee.Age == 20 || employee.Age == 30 || employee.Age == 35).Select(employee => employee);
-            Console.WriteLine("Employees that aren't 20, 30 or 35 years old:");
-            foreach(var result4 in notTheseAges)
-            {
-                Console.WriteLine($"Name: {result4.Name}, Age: {result4.Age}");
+                Console.WriteLine($"Name: {item.LineManager}, Roll Number: {item.groupedNumber}");
             }
 
-            Console.WriteLine();
-
-            var idOfOneToEight = employees.Where(employee => employee.Id >= 1 && employee.Id <= 8).Select(employee => employee);
-            Console.WriteLine("Employees of IDs betwee 1 and 8");
-            foreach(var result5 in idOfOneToEight)
+            foreach (var employee in groupJoinQuery)
             {
-                Console.WriteLine($"Name: {result5.Name}, Age: {result5.Id}");
+                Console.WriteLine($"{employee.LineManager} manages the following employees:");
+                foreach (var under in employee.groupedEmployees)
+                {
+                    Console.WriteLine($"Name: {under.Name}, Roll Number {under.RollNumber}");
+                }
+                Console.WriteLine();
+            }
+
+
+            //Group join: extenstion method syntax
+            var groupJoinExtension = lineManager.GroupJoin(employees, x => x.Id, y => y.Designation, (x, groupedEmployees) => new
+            {
+                x.LineManager,
+                groupedEmployees
+            });
+
+            foreach (var employee in groupJoinExtension)
+            {
+                Console.WriteLine($"{employee.LineManager} manages the following employees:");
+                foreach (var under in employee.groupedEmployees)
+                {
+                    Console.WriteLine($"Name: {under.Name}, Roll Number {under.RollNumber}");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            //Join or inner join: Using the query syntax
+            var innerJoinQuery = from employee in employees
+                                 join manager in lineManager
+                                 on employee.Designation
+                                 equals manager.Id
+                                 select new
+                                 {
+                                     employee.Name,
+                                     manager.LineManager,
+                                     employee.Gender
+                                 };
+
+
+            //Join or inner join: Using the extension method syntax
+            var innerJoinExtension = employees.Join(lineManager, x => x.Designation, y => y.Id, (x, y) => new
+            {
+                x.Name,
+                y.LineManager,
+                y.SalaryAmount
+            }).Where(y => y.SalaryAmount < 5000).OrderByDescending(x => x.Name);
+
+            foreach (var line in innerJoinExtension)
+            {
+                Console.WriteLine($"Employee name: {line.Name}, Line Manager: {line.LineManager}, Designation: {line.SalaryAmount}");
             }
         }
-    }
-     
-    public class Items
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-        
-        public double Price { get; set; }
     }
 }
 
