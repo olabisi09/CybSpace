19003a2fc49422ca6d419110555536f38a7a5f97
olabisi09 <olabisiolaoye09@gmail.com>
2022-04-29 19:59:16 +0100

    LINQ assignment for the weekend #1

--- Cyberspace.Starter/Employee.cs
+++ Cyberspace.Starter/Employee.cs
@@ -8,26 +8,26 @@ namespace Cyberspace.Starter
 {
     public class Employee
     {
-        public int RollNumber { get; set; }
-        public string Name { get; set; }    
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public int Age { get; set; }
-        public Gender Gender { get; set; }
-        public int Designation { get; set; }
 
         public static List<Employee> GetEmployees()
         {
             return new List<Employee>
             {
-                new Employee() { RollNumber = 101, Name = "Monica Geller", Age = 23, Gender = Gender.female, Designation = 3},
-                new Employee() { RollNumber = 102, Name = "Ross Geller", Age = 28, Gender = Gender.male, Designation = 3},
-                new Employee() { RollNumber = 103, Name = "Phoebe Buffay", Age = 30, Gender = Gender.female, Designation = 1},
-                new Employee() { RollNumber = 104, Name = "Chandler Bing", Age = 29, Gender = Gender.male, Designation = 2},
-                new Employee() { RollNumber = 105, Name = "Joey Tribbiani", Age = 35, Gender = Gender.male, Designation = 4},
-                new Employee() { RollNumber = 106, Name = "Eddie Moskowitz", Age = 32, Gender = Gender.male, Designation = 1},
-                new Employee() { RollNumber = 107, Name = "Joshua Burgen", Age = 40, Gender = Gender.male, Designation = 4},
-                new Employee() { RollNumber = 108, Name = "Janice Hosenstein", Age = 34, Gender = Gender.female, Designation = 2},
-                new Employee() { RollNumber = 109, Name = "Will Culvert", Age = 26, Gender = Gender.male, Designation = 2},
-                new Employee() { RollNumber = 110, Name = "Rachel Green", Age = 30, Gender = Gender.female, Designation = 3}
+                new Employee { FirstName = "Jude", LastName = "Smith", Age = 41},
+                new Employee { FirstName = "Sarah", LastName = "Jones", Age = 22},
+                new Employee { FirstName = "Stacy", LastName = "Baker", Age = 21},
+                new Employee { FirstName = "Samuel", LastName = "Dexter", Age = 17},
+                new Employee { FirstName = "Richy", LastName = "Smith", Age = 49},
+                new Employee { FirstName = "Brett", LastName = "Joy", Age = 51},
+                new Employee { FirstName = "Mark", LastName = "Parker", Age = 116},
+                new Employee { FirstName = "Alice", LastName = "Thompson", Age = 18},
+                new Employee { FirstName = "Evelyn", LastName = "Winsome", Age = 58 },
+                new Employee { FirstName = "Mort", LastName = "Martin", Age = 58},
+                new Employee { FirstName = "Eugene", LastName = "deLauter", Age = 84},
+                new Employee { FirstName = "Gail", LastName = "Dude", Age = 34}
             };
         }
     }
--- Cyberspace.Starter/Program.cs
+++ Cyberspace.Starter/Program.cs
@@ -8,16 +8,61 @@ namespace Cyberspace.Starter
     {
         static void Main()
         {
-            //element operations in LINQ
+            string[] keywords = { "abstract", "add*", "alias*", "as", "ascending*", "async*", "await", "base", "bool", "break", "by*", "byte", "case", "catch", "char", "checked", "class", "const", "continue", "decimal", "default", "delegate", "descending*", "do", "double", "dynamic*", "else", "enum", "event", "equals", "explicit", "extern", "false", "finally", "fixed", "from*", "float", "for", "foreach", "get*", "global*", "group*", "goto", "if", "implicit", "in", "int", "into*", "interface", "internal", "is", "lock", "long", "join*", "let*", "nameof*", "namespace", "new", "null", "object", "on*", "operator", "orderby*", "out", "override", "params", "partial*", "private", "protected", "public", "readonly", "ref", "remove*", "return", "sbyte", "sealed", "select*", "set*", "short", "sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unsafe", "ushort", "using", "value*", "var*", "virtual", "unchecked", "void", "volatile", "where*", "while", "yield*"};
 
-            //First(), FirstOrDefault(), Last(), LastOrDefault()
-            int[] numbers = {1, 2, 3, 4, 5, 6};
-            Console.WriteLine(numbers.LastOrDefault());
+            //Question 1 - 3
+            var containsE = keywords.Where(x => x.Contains('e')).Select(x => x).OrderBy(x => x);
 
-            //Single(), SingleOrDefault()
-            var some = numbers.Single();
-            var all = numbers.SingleOrDefault(x => x >= 3);
-            Console.WriteLine(all);
+            Console.WriteLine("Words that contain the letter 'e' include: ");
+            foreach (var x in containsE)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+            
+            //Question 4
+            var last = keywords.Last();
+            Console.WriteLine($"Last word in the list: {last}");
+            Console.WriteLine();
+
+            //Question 5
+            var containsAsterisk = keywords.Where(x => x.Contains('*')).Select(x => x).OrderBy(x => x);
+            Console.WriteLine("Words that contain '*' include: ");
+            foreach (var x in containsAsterisk)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+
+            var employees = Employee.GetEmployees();
+
+            var lastNamesD = employees.Where(x => x.LastName.StartsWith("d", StringComparison.CurrentCultureIgnoreCase)).Select(x => x);
+            Console.WriteLine("Employees with last names starting with D:");
+            foreach (var employee in lastNamesD)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+            }
+            Console.WriteLine();
+
+            var olderThan40 = employees.Where(x => x.Age > 40).OrderByDescending(x => x.FirstName);
+            Console.WriteLine("Employees older than 40:");
+            foreach (var employee in olderThan40)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+            }
+            Console.WriteLine();
+
+            var firstPersonOlderThan40 = olderThan40.First();
+            Console.WriteLine($"First person older than 40 in descending order: {firstPersonOlderThan40.FirstName} {firstPersonOlderThan40.LastName}\n");
+
+            var words = new List<string>() { "plane", "ferry", "car", "bike" };
+            words.Sort();
+            var containsEAgain = words.Where(x => x.Contains("e")).Select(x => x);
+            Console.WriteLine("Words that contain the letter 'e':");
+            foreach (var word in containsEAgain)
+            {
+                Console.WriteLine(word);
+            }
         }
     }
 }
