56a63570bf1c083190bd164e39a1da7f72f98156
olabisi09 <olabisiolaoye09@gmail.com>
2022-05-13 14:08:16 +0100

    reading and writing data

--- Cyberspace.Starter/Program.cs
+++ Cyberspace.Starter/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Cyberspace.Starter
@@ -9,100 +11,91 @@ namespace Cyberspace.Starter
     {
         static void Main()
         {
-            //AccountTask();
-            //DaysOfTheWeek();
-            MonthsOfTheYear();
-        }
+            goto label1;
+            var x = new DirectoryInfo(".");
+            var dir = new DirectoryInfo(@"C:\Users\DELL\Pictures\testingDocument");
+            Console.WriteLine(x.Name);
+            Console.WriteLine(dir.Name);
+            Console.WriteLine(dir.FullName);
+            Console.WriteLine(dir.Parent);
+            Console.WriteLine(dir.CreationTime);
 
-        static void AccountTask()
-        {
-            var accounts = Account.GetAccounts();
-            Task.Run(() =>
-            {
-                var result = accounts.Sum(a => a.Amount);
-                Console.WriteLine($"Sum of amount: {result}");
-            });
-            Task.Run(() =>
-            {
-                var names = accounts.Select(a => a.Name);
-                Console.WriteLine("Account holder names:");
-                foreach (var name in names)
-                {
-                    Console.WriteLine(name);
-                }
-            });
-            Console.Read();
-        }
+            string[] words = { "Aule the smith", "Yavanna of the trees", "Manwe of the air", "Mandos" };
+            string path = @"C:\Users\DELL\Pictures\testingDocument";
 
-        static void DaysOfTheWeek()
-        {
-            var days = new List<string> { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" }.ToList();
-            days.Sort();
-            Console.WriteLine("Days of the week in ascending order: ");
-            foreach (var day in days)
+            //File.AppendAllLines(path, words);
+            //foreach (var line in File.ReadAllLines(path))
+            //{
+            //    Console.WriteLine(line);
+            //}
+
+            var textFile = dir.GetFiles("*.txt", SearchOption.AllDirectories);
+            foreach (var text in textFile)
             {
-                Console.WriteLine(day);
+                Console.WriteLine(text.FullName);
+                Console.WriteLine(text.CreationTime);
             }
-            Console.WriteLine();
-        }
 
-        static void MonthsOfTheYear()
-        {
-            var months = new List<string> { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
-            //var newstuff = months.TakeLast(2);
-            Console.WriteLine("Months of the year: ");
-            foreach (var month in months)
-            {
-                //Console.WriteLine(month != months.Last() ? month + ", " : month);
-                Console.WriteLine(month);
-            }
-            Console.WriteLine();
+            WriteStuff();
+            ReadStuff();
+        label1:
+            UseFileStreamWrite();
             
-            Console.WriteLine($"Last two months of the year are {months[months.Count - 2]} and {months[months.Count - 1]}");
         }
-    }
 
-    public class Account
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Gender { get; set; }
-        public decimal Amount { get; set; }
-        public string YearOfSpecification { get; set; }
-
-        public static List<Account> GetAccounts()
+        static void UseFileStreamWrite()
         {
-            return new List<Account>
+            string path = @"C:\Users\DELL\Pictures\testingDocument\testing.txt";
+            var fileStream = File.Open(path, FileMode.Create);
+            string word = "Ainulindale: the first book in the Silmarillion";
+
+            byte[] list = Encoding.Default.GetBytes(word);
+            fileStream.Write(list, 0, list.Length);
+
+            fileStream.Position = 0;
+            byte[] fileList = new byte[list.Length];
+            for(int i = 0; i < list.Length; i++)
             {
-                new Account { Id = 2, Name = "Amar", Gender = "F", Amount = 400000, YearOfSpecification = "2 years"},
-                new Account { Id = 6, Name = "Joe", Gender = "M", Amount = 40000, YearOfSpecification = "2 monnths"},
-                new Account { Id = 8, Name = "Tola", Gender = "F", Amount = 800000, YearOfSpecification = "5 days"},
-                new Account { Id = 10, Name = "Jude", Gender = "M", Amount = 500000, YearOfSpecification = "6 years"},
-                new Account { Id = 12, Name = "Amir", Gender = "F", Amount = 1000000, YearOfSpecification = "1 year"},
-                new Account { Id = 14, Name = "Sam", Gender = "F", Amount = 600000, YearOfSpecification = "1 year"},
-                new Account { Id = 16, Name = "Joy", Gender = "F", Amount = 100000, YearOfSpecification = "3 years"},
-                new Account { Id = 18, Name = "Ola", Gender = "M", Amount = 20000, YearOfSpecification = "4 months"}
-            };
-        }
-    }
-
-    public class Calculator
-    {
-        //[Obsolete("This method is obsolete. Try again suckers!")]
-        public static int Add(int a, int b)
-        {
-            //Console.WriteLine("MEthod outdated");
-            return a + b;
-        }
-
-        public static int Add(List<int> numbers)
-        {
-            int result = 0;
-            foreach (int i in numbers)
-            {
-                result += i;
+                fileList[i] = (byte)fileStream.ReadByte();
             }
-            return result;
+            Console.WriteLine(Encoding.Default.GetString(fileList));
+            fileStream.Close();
+        }
+
+        static void WriteStuff()
+        {
+            var streamWriter = new StreamWriter(@"C:\Users\DELL\Pictures\testingDocument\testing.txt");
+            streamWriter.WriteLine("Behold the Valar!");
+            streamWriter.WriteLine("Manwe");
+            streamWriter.Close();
+        }
+
+        static void ReadStuff()
+        {
+            var streamWriter = new StreamReader(@"C:\Users\DELL\Pictures\testingDocument\testing.txt");
+            Console.WriteLine(streamWriter.ReadToEnd());
+            streamWriter.Close();
+        }
+    }
+
+    public class StudentData
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Class { get; set; }
+        public decimal Grade { get; set; }
+
+        public List<StudentData> Students()
+        {
+            return new List<StudentData>
+            {
+                new StudentData { Name = "Phoebe", Age = 31, Class = "400 level", Grade = 95 },
+                new StudentData { Name = "Ross", Age = 28, Class = "200 level", Grade = 70 },
+                new StudentData { Name = "Monica", Age = 30, Class = "500 level", Grade = 82 },
+                new StudentData { Name = "Chandler", Age = 25, Class = "100 level", Grade = 95 },
+                new StudentData { Name = "Joey", Age = 26, Class = "300 level", Grade = 63 },
+                new StudentData { Name = "Rachel", Age = 25, Class = "100 level", Grade = 56 }
+            };
         }
     }
 }
